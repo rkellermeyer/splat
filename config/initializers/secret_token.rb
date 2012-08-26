9397df7db6d59e667b920c1fc3c945b5e11056bf
Richard Kellermeyer <Dick@Richards-MacBook-Pro.local>
2012-08-26 18:06:33 -0400

    Sync

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splat::Application.config.secret_token = '7189e36e7163452721b01c83b1834efb8af134d1e2aad1eef51eb6c0ffeb4b3a6f4d8ff92222c34cfce59085374136cf3b70e64c45f7f4838463b0a0a0250fd4'
